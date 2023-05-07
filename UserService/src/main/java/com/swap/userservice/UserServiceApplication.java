831048546f708023fb5a7b84edf123e67afcbfcf
swapnil nyayade <swapnilnyayade040@gmail.com>
2023-05-07 19:08:20 +0530

    use Service Name

--- UserService/src/main/java/com/swap/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/swap/userservice/UserServiceApplication.java
@@ -2,10 +2,17 @@ package com.swap.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableEurekaClient is not imported/supported(no need to add)
 public class UserServiceApplication {
+	
+//	@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
--- UserService/src/main/java/com/swap/userservice/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/swap/userservice/impl/UserServiceImpl.java
@@ -1,11 +1,22 @@
 package com.swap.userservice.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.swap.userservice.entities.Hotel;
+import com.swap.userservice.entities.Rating;
 import com.swap.userservice.entities.User;
 import com.swap.userservice.exceptions.ResourceNotFoundException;
 import com.swap.userservice.repositories.UserRepository;
@@ -16,6 +27,11 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
 	@Override
 	public User saveUser(User user) {
@@ -36,7 +52,38 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getUser(String userId) {
 		// TODO Auto-generated method stub
-		return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!! " + userId));
+		
+		//get user from database with the help of user repository
+		User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server!! " + userId));
+		
+		//fetch rating of the above user from RATING SERVICE
+		//http://localhost:8083/ratings/users/8b24520e-72ea-429d-8447-ef68bc56a18c
+//		ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + userId, ArrayList.class);
+//		logger.info("{} ",ratingsOfUser);
+		//In the above method it creates problem while using map on ratingsOfUser as response is converted into ArrayList but Rating is not identified(it kept data in linked hashset format)
+		 
+		Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/" + userId, Rating[].class);
+		logger.info("{} ",ratingsOfUser);
+		
+		List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+		
+		List<Rating> ratingList = ratings.stream().map(rating -> {
+		//api call to HOTEL SERVICE to get the hotel
+		//http://localhost:8082/hotels/fb41df3a-8dac-4a38-9b41-e8d69255a2f6
+		ResponseEntity<Hotel> forEntity	= restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/" + rating.getHotelId(), Hotel.class);
+		Hotel hotel = forEntity.getBody();
+		
+		HttpStatusCode statusCode = forEntity.getStatusCode();
+		logger.info("response status code {} " + statusCode);
+		
+		rating.setHotel(hotel);
+		
+		return rating;
+		}).collect(Collectors.toList());
+		
+		user.setRatings(ratingList);
+		return user;
+	
 	}
 
 	@Override
